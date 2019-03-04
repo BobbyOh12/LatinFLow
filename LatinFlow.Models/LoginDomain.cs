7be04e40e9da7744cbfea2ce3061352f5f6e8432
Bobby.Oh <bawbee.oh@gmail.com>
2019-03-03 20:22:57 -0800

    savae

---
+++ LatinFlow.Models/LoginDomain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatinFlow.Models
+{
+    public class LoginDomain
+    {
+        public string Email { get; set; }
+        public string Salt { get; set; }
+        public string PasswordHash { get; set; }
+        public int RelatedUserId { get; set; }
+        public DateTime LastLogin { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public string ModifiedBy { get; set; }
+    }
+}
---
+++ LatinFlow.Models/Requests/UserLoginAddRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatinFlow.Models.Domain
+{
+    public class UserLoginAddRequest
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [MaxLength(1)]
+        [Display(Name = "Middle Initial")]
+        public string MiddleInitial { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
